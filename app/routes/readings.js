fec1ac202bdde218e122bf5190ac634de3871cf4
maliaedmonds <maliaedmonds@arizona.edu>
2024-11-18 17:19:53 -0700

    fix readings/addData

--- app/routes/readings.js
+++ app/routes/readings.js
@@ -9,15 +9,18 @@ var Reading = require("../models/reading");
 // see Figure 9.3.5: Node.js project uses token-based authentication and password hashing with bcryptjs on zybooks
 
 router.post('/addData', function (req, res) {
+    let data = JSON.parse(req.body.data);
     console.log(req.body);
-    if (!req.body.ir || !req.body.heartRate || !req.body.spo2) {
+    console.log("data: " + data);
+    if (!data.ir || !data.heartRate || !data.spo2) {
         res.status(401).json({ error: "Missing data" });
         return;
     }
     const newReading = {
-        ir: req.body.ir,
-        heartRate: req.body.heartRate,
-        spo2: req.body.spo2
+        ir: data.ir,
+        heartRate: data.heartRate,
+        spo2: data.spo2,
+        deviceID: req.body.coreid
     };
     newReading.save().then((reading) => {
         // Send back a token that contains the user's username
@@ -25,6 +28,20 @@ router.post('/addData', function (req, res) {
     }).catch((err) => {
         res.status(400).json({ success: false, err: err });
     });
+});
+
+router.get("/getData", function (req, res) {
+    try {
+        // Send back email and last access
+        Reading.find({}).then((user) => {
+            res.status(200).json({email: user.email, lastAccess: user.lastAccess});
+        }).catch((err) => {
+            res.status(400).json({ success: false, message: "Error contacting DB. Please contact support." });
+        });
+    }
+    catch (ex) {
+        res.status(401).json({ success: false, message: "Invalid JWT" });
+    }
 });
 
 module.exports = router;
